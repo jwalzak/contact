a31455876c7fdcae9f1fe88431d5d3bc7f152abb
Jason Walzak <walzak@gmail.com>
2017-04-07 01:07:52 -0400

    Style changes
    Put some styling into the page. [
    SVG background
    When you click the update button it logs the id of the hidden input to
    the console.
    This will make it easy to update later.
    //TODO: turn the email into a mailto:
    //This about turning the phone numbers into clickable links.

--- index.php
+++ index.php
@@ -7,35 +7,44 @@ require_once("Connect.php");
 <head>
     <meta charset="UTF-8">
     <title>CONTACTOR</title>
-    <link rel="stylesheet" href="css/style.css" />
+
+    <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/svg.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- <script type="text/javascript" src="/css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script> -->
     <script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
     <script type="text/javascript" src="./js/main.js"></script>
 </head>
 <body>
-    <header class="top">
-        <h1 class="title" id="title">Contactor, the web's formost Contact getting bot!</h1>
-    </header>
-    <main>
-        <form action="">
-            <label for="search">Search by Full name:</label><input name="search_input" id="search" type="text">
-            <input type="submit" value="Search">
-        </form>
-        <form action="">
-            <input type="text" name="firstName"><label for="firstName">First Name</label><br />
-            <input type="text" name="lastName"><label for="lastName">Last Name</label><br />
-            <input type="text" name="city"><label for="city">City</label><br />
-            <input type="text" name="region"><label for="region">Region</label><br />
-            <input type="text" name="emailOne"><label for="emailOne">Email 1</label><br />
-            <input type="text" name="emailTwo"><label for="emailTwo">Email 2</label><br />
-            <input type="text" name="phoneNumOne"><label for="phoneNumOne">Phone Number 1</label><br />
-            <input type="text" name="PhoneNumTwo"><label for="phoneNumTwo">Phone Number 2</label><br />
-            <input type="submit">
-        </form>
+<div class="container-fluid">
+    <div class="col-md-12">
+        <header class="top">
+            <h1 class="title" id="title">Contactor, the web's formost Contact getting bot!</h1>
+        </header>
+    </div>
+    <div class="col-md-8">
+        <main>
+            <form action="">
+                <label for="search">Search by Full name:</label><input name="search_input" id="search" type="text">
+                <input type="submit" value="Search">
+            </form>
+            <form action="">
+                <input type="text" name="firstName"><label for="firstName">First Name</label><br />
+                <input type="text" name="lastName"><label for="lastName">Last Name</label><br />
+                <input type="text" name="city"><label for="city">City</label><br />
+                <input type="text" name="region"><label for="region">Region</label><br />
+                <input type="text" name="emailOne"><label for="emailOne">Email 1</label><br />
+                <input type="text" name="emailTwo"><label for="emailTwo">Email 2</label><br />
+                <input type="text" name="phoneNumOne"><label for="phoneNumOne">Phone Number 1</label><br />
+                <input type="text" name="PhoneNumTwo"><label for="phoneNumTwo">Phone Number 2</label><br />
+                <input type="submit" class="btn btn-info btn-large">
+            </form>
+    </div>
 
-        <div id="contacts" class="contacts">
-            
-        </div>
-    </main>
+            <div id="contacts" class="contacts col-md-12"></div>
+        </main>
+    </div>
+
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -13,30 +13,38 @@ function loadContact(){
 
 function displayContacts(contact){
     for(let i = 0; i<contact.length; i++){
-        let $contactDiv = $("<div>").addClass('singleContact').attr('id', contact[i].id);
-        let $userName = $("<p>").text("Name: " + contact[i].addr_first_name + contact[i].addr_last_name);
-        let $userAddress = $("<p>").text("City & Region: " + contact[i].addr_city);
+        let $contactDiv = $("<div>").addClass('singleContact col-md-3').attr('id', contact[i].id);
+        let $userName = $("<p>").text("Name: " + contact[i].addr_first_name + " " + contact[i].addr_last_name);
+        let $userAddress = $("<p>").text("City & Region: " + contact[i].addr_city + " " + contact[i].addr_region);
         let $userEmail = $("<p>").text("Email Address: " + contact[i].addr_email_1);
         let $userPhone = $("<p>").text("Phone Number: " + contact[i].addr_phone_1);
         $contactDiv.append($userName).append($userAddress).append($userEmail);
         
-        if(contact[i].addr_email_2 == "null"){
+        if(contact[i].addr_email_2 != null){
             let $emailTwo = $("<p>").text("Work Email: " + contact[i].addr_email_2);
             $contactDiv.append($emailTwo);
         }//End if
 
         $contactDiv.append($userPhone);
 
-        if(contact[i].addr_phone_2 == "null"){
+        if(contact[i].addr_phone_2 != null){
             let $phoneTwo = $("<p>").text("Home Phone: " + contact[i].addr_phone_2);
             $contactDiv.append($phoneTwo);
         }//End if
-        let $edit = $("<button>").addClass('button').text("Update Contact");
-        $contactDiv.append($edit);
+        let $edit = $("<button>").attr("id", "button-" + contact[i].addr_id).addClass('btn btn-info btn-lg').text("Update Contact");
+        let $contactId = $("<input>").attr({id: "input-" + contact[i].addr_id, type: "hidden"}).val(contact[i].addr_id);
+        $contactDiv.append($edit).append($contactId);
 
         $("#contacts").append($contactDiv);
         console.log(contact[i]);
+        $("#button-" + contact[i].addr_id).click(function(){
+            update($(this).siblings('input').val());
+        });
     }//End for
 }//End displayContacts();
 
+//Update contact function
+function update(updateId){
+    console.log(updateId);
+}//End update();
 
